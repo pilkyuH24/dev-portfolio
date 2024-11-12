47873fd7d32c7b364f778eebbadbb17c01770266
Pilkyu Han <pil331@berkeley.edu>
2024-11-12 02:15:33 -0800

    Fix: Notion api nextconfig changed

--- next.config.mjs
+++ next.config.mjs
@@ -2,12 +2,13 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ['images.unsplash.com', 'www.notion.so'],
-  },
-  env: {
-    NOTION_DATABASE_ID: process.env.NOTION_DATABASE_ID,
-    NOTION_TOKEN: process.env.NOTION_TOKEN,
-  },
-};
+      domains: [
+          'www.notion.so',
+          'images.unsplash.com',
+          's3.us-west-2.amazonaws.com'
+      ],
+      format: ['image/png', 'image/webp', 'image/jpeg']
+  }
+}
 
-export default nextConfig;
+module.exports = nextConfig
