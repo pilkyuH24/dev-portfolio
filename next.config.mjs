9e096bddb74dd75b5cede024728aad9ef376a319
Pilkyu Han <pil331@berkeley.edu>
2024-11-21 04:04:24 -0800

    notion uploaded image format

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,7 @@ const nextConfig = {
   reactStrictMode: true,
   images: {
     domains: ['www.notion.so', 'images.unsplash.com', 's3.us-west-2.amazonaws.com'],
-    formats: ['image/avif', 'image/webp'],
+    formats: ['image/avif', 'image/webp','image/png', 'image/webp', 'image/jpeg'],
   },
 };
 
