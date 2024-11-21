dfee6f2048570cc4dcd90c05b99f6af6da433adf
Pilkyu Han <pil331@berkeley.edu>
2024-11-21 04:11:55 -0800

    notiaon api updated

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,12 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ['www.notion.so', 'images.unsplash.com', 's3.us-west-2.amazonaws.com'],
-    formats: ['image/avif', 'image/webp','image/png', 'image/webp', 'image/jpeg'],
+    domains: [
+      "www.notion.so",
+      "images.unsplash.com",
+      "s3.us-west-2.amazonaws.com",
+    ],
+    formats: ["image/avif", "image/webp", "image/png", "image/jpeg"],
   },
 };
 
